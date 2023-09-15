75d4dad156eabd43fbf0f873805d0704654dcf2d
IgorMushk <igor.mushketik@gmail.com>
2023-09-15 10:07:47 +0300

    Add Searchbar.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,11 @@
 import React, { Component } from 'react'
+import Searchbar from './Searchbar/Searchbar'
 
 export class App extends Component {
   render() {
     return (
-      <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template! (to class App)
+     <div>
+     <Searchbar/>
     </div>
     )
   }
